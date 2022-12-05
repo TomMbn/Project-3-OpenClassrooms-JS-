6f3f61bb69ac3bc75eddce577cd14b502a92214f
Tom Mauboussin <tom.mauboussin@icloud.com>
2022-12-05 19:51:32 +0100

    Ajout des filtres des travaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,68 @@
 let api = "http://localhost:5678/api/";
 let urlWorks = api+"works";
+let urlCategory = api+"categories";
 let gallery = document.querySelector(".gallery");
+let filter = document.querySelector("#filterList")
+const categorySet = new Set();
 
+async function getCategory(){
+    const request = await fetch(urlCategory, {
+        method : "GET"
+    });
+
+    if (!request.ok){
+        alert("Une erreur est survenue");
+    }
+    else{
+        let donnees = await request.json();
+        donnees.forEach(element =>{
+            categorySet.add(element.name);
+        })
+        categorySet.forEach(element => {
+        addFilter(element);
+        });
+    }
+}
+
+function addFilter(category){
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "filter");
+    newDiv.textContent = category;
+    newDiv.addEventListener("click", eventFilter);
+    document.querySelector("#filterList").appendChild(newDiv);
+}
+
+function eventFilter(){
+    let galleryFigures = gallery.querySelectorAll("figure");
+    let filterDiv = document.getElementsByClassName("filter");
+
+    this.style.color = "white";
+    this.style.backgroundColor = "#1D6154";
+
+    for (let i = 0; i < filterDiv.length; i++) {
+        if(!(filterDiv[i].textContent === this.textContent)){
+            filterDiv[i].style.color = "#1D6154";
+            filterDiv[i].style.backgroundColor = "white";
+        }
+    }
+    
+    if (this.textContent === "Tous"){
+        for (let i = 0; i < galleryFigures.length; i++) {
+                galleryFigures[i].style.display = "block";
+        }
+    }
+    else{ 
+        for (let i = 0; i < galleryFigures.length; i++) {
+            if(galleryFigures[i].className === this.textContent){
+                galleryFigures[i].style.display = "block";
+            }
+            else{
+                galleryFigures[i].style.display = "none";
+            }
+            
+        }
+    }
+}
 
 async function getWorks(){
     const request = await fetch(urlWorks, {
@@ -13,6 +74,7 @@ async function getWorks(){
     }
     else{
         let donnees = await request.json();
+        
         donnees.forEach(element => {
             addWorks(element);
         });
@@ -22,8 +84,11 @@ async function getWorks(){
 function addWorks(work){
     let image = work.imageUrl; 
     let nom = work.title;
+    let category = work.category.name;
+    console.log(category);
 
     let newFigure = document.createElement("figure");
+    newFigure.className = category;
     
     let newImg = document.createElement("img");
     newImg.setAttribute("src", image);
@@ -38,4 +103,7 @@ function addWorks(work){
     gallery.appendChild(newFigure);
 }
 
+
 getWorks();
+addFilter("Tous");
+getCategory();
